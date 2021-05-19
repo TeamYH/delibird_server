4ce210ad6c3818fe2ff5ad2abb06b6de5f4f3542
배해진 <bae05030@ajou.ac.kr>
2021-05-20 04:12:06 +0900

    modify models and add to admin

--- delibird_db/admin.py
+++ delibird_db/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from delibird_db.models import Customer, Store, Robot, Table
 # Register your models here.
+
+admin.site.register(Customer)
+admin.site.register(Store)
+admin.site.register(Robot)
+admin.site.register(Table)
+ 
--- delibird_db/models.py
+++ delibird_db/models.py
@@ -1,14 +1,30 @@
 from django.db import models
+# from django.contrib.auth.models import User
 import json
+# from django.dispatch import receiver
+# from django.db.models.signals import post_save
 
 # Create your models here.
-class User(models.Model):
+class Customer(models.Model):
+    # user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=30, null=False)
     phonenum = models.CharField(max_length=15, null=False)
+    customer_id = models.CharField(max_length=12, null=False)
+    customer_password = models.CharField(max_length=12, null=False)
+
+    # @receiver(post_save, sender=User)
+    # def create_user_profile(sender, instance, created, **kwargs):
+    #     if created:
+    #         Customer.objects.create(user=instance)
+
+    # @receiver(post_save, sender=User)
+    # def save_user_profile(sender, instance, **kwargs):
+    #     instance.Customer.save()
+
 
 
 class Store(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(Customer, on_delete=models.CASCADE)
     store_name = models.CharField(max_length=50, null=False)
     address = models.TextField(null=False)
     detail_address = models.CharField(max_length=30, null=False)
@@ -17,31 +33,31 @@ class Store(models.Model):
 
 class Robot(models.Model):
     store = models.ForeignKey(Store, on_delete=models.CASCADE, null=True)
-    ROBOT_STATUS = (
-            ('R','require_setting'),
-            ('M', 'mapping'),
-            ('I', 'idle'),
-            ('S', 'serving'),
-            ('R', 'returning'),
-            ('C', 'cleaning'),
-    )
+    # ROBOT_STATUS = (
+    #         ('R','require_setting'),
+    #         ('M', 'mapping'),
+    #         ('I', 'idle'),
+    #         ('S', 'serving'),
+    #         ('R', 'returning'),
+    #         ('C', 'cleaning'),
+    # )
     serial_num = models.CharField(max_length=20, primary_key=True)
-    status = models.CharField(max_length=1, choices=ROBOT_STATUS, default='R', null=False)
-    battery = models.IntegerField(null=False)
+    # status = models.CharField(max_length=1, choices=ROBOT_STATUS, default='R', null=False)
+    # battery = models.IntegerField(null=False)
     desc = models.TextField(null=True)
-    current_pos_x = models.IntegerField(null=True)
-    current_pos_y = models.IntegerField(null=True)
+    # current_pos_x = models.IntegerField(null=True)
+    # current_pos_y = models.IntegerField(null=True)
 
 
-class Map(models.Model):
-    store = models.OneToOneField(Store, on_delete=models.CASCADE, null=False)
-    date = models.DateField(null=False)
-    map_key = models.CharField(max_length=1000)
+# class Map(models.Model):
+#     store = models.OneToOneField(Store, on_delete=models.CASCADE, null=False)
+#     date = models.DateField(null=False)
+#     map_key = models.CharField(max_length=1000)
 
 
 class Table(models.Model):
     store = models.ForeignKey(Store, on_delete=models.CASCADE, null=False)
-    table_num = models.IntegerField(null=False)
+    # table_num = models.IntegerField(null=False, primary_key=True)
     pos_x = models.IntegerField(null=False)
     pos_y = models.IntegerField(null=False)
     angle = models.IntegerField(null=False)
